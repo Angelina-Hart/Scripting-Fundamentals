0d0bbe6639d42ba1b20a3fb774cdb441bd53db61
Angelina-Hart <151281092+Angelina-Hart@users.noreply.github.com>
2023-11-30 22:06:00 -0500

    Create main.py

---
+++ Module 8/Module Lab Assessment 8 Mailing List Validation File Processing/main.py
@@ -0,0 +1,17 @@
+# Import mailinglist_validation_util from your package
+from your_package import your_module
+
+
+def mailing_list_utils():
+     """
+       Your docstring documentation starts here.
+
+    """
+
+    return # Returning the output of the `mailinglist_validation_util` function
+
+
+if __name__ == '__main__':
+
+    # Calling the function from your package
+    print('The output file has length {}.'.format(mailing_list_utils_extended()))
